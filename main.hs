bc1bc8c14fb912a9200dc9489eddbd40ba07f8e8
Danilo Alexandre <daniloabs@id.uff.br>
2026-05-09 16:59:33 -0300

    Colocando tudo em inglês e no CamelCase

--- main.hs
+++ main.hs
@@ -4,29 +4,29 @@ import System.IO
 main::IO()
 
 -- Nome do arquivo de entrada para as palavras reservadas
-nomeArquivoRes::String
-nomeArquivoRes = "res.txt"
+nameFileRes::String
+nameFileRes = "res.txt"
 
 -- Nome do arquivo de entrada para os separadores
-nomeArquivoSep::String
-nomeArquivoSep = "sep.txt"
+nameFileSep::String
+nameFileSep = "sep.txt"
 
 -- Nome do arquivo de entrada para o primeiro programa
-nomeArquivoC1::String
-nomeArquivoC1 = "c1.txt"
+nameFileC1::String
+nameFileC1 = "c1.txt"
 
 -- Nome do arquivo de entrada para o segundo programa
-nomeArquivoC2::String
-nomeArquivoC2 = "c2.txt"
+nameFileC2::String
+nameFileC2 = "c2.txt"
 
 -- Função para ler o conteúdo de um arquivo e retornar como uma lista de strings
-lePalavrasArquivo::String -> IO [String]
-lePalavrasArquivo nomeArquivo = do
-    conteudo <- readFile nomeArquivo
-    return (words conteudo)
+readWordsFile::String -> IO [String]
+readWordsFile fileName = do
+    content <- readFile fileName
+    return (words content)
 
 -- Função main
 main = do
     putStrLn "Palavras reservadas lidas:"
-    palavrasReservadas <- lePalavrasArquivo nomeArquivoRes
-    print palavrasReservadas
+    reservedWords <- readWordsFile nameFileRes
+    print reservedWords
